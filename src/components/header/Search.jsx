02b04451708cce7bbb1ae5efbceb199fd6e995b7
jtr-code <jtrtricks@gmail.com>
2023-01-03 19:31:45 +0530

    Input field and Search icon added

---
+++ src/components/header/Search.jsx
@@ -0,0 +1,44 @@
+import { InputBase, Box, styled } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+//      <--------------------------------------- styled section starts-------------------------------->
+
+const SearchContainer = styled(Box)`
+  display: flex;
+  width: 521.9998px;
+  height: 36px;
+  color: #000000;
+  background-color: #ffff;
+  padding: 0 16px 0 16px;
+  margin-left: 12px;
+  font-size: 14px;
+  border-radius: 2px;
+  align-items: center;
+`;
+
+const InputSearchBase = styled(InputBase)`
+  padding: 0;
+  width: 100%;
+  font-size: 13.33px;
+  font-family: Roboto, Arial, sans-serif;
+`;
+
+const SearchIconWrapper = styled(Box)`
+  color: #2874f0;
+  cursor: pointer;
+`;
+
+//      <--------------------------------------- styled section ends-------------------------------->
+
+const Search = () => {
+  return (
+    <SearchContainer>
+      <InputSearchBase placeholder="Search for products, brands and more" />
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+    </SearchContainer>
+  );
+};
+
+export default Search;
