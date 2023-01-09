10b7df0f98e6767e0dd88a87330ea6e94d4ba29a
jtr-code <jtrtricks@gmail.com>
2023-01-09 21:15:45 +0530

    Added proper margin left and right width

--- src/components/home/NavBar.jsx
+++ src/components/home/NavBar.jsx
@@ -1,16 +1,31 @@
+import { styled } from "@mui/material";
 import { Box } from "@mui/material";
 import { navData } from "../../constants/data";
 
+//      <--------------------------------------- styled section starts-------------------------------->
+
+const Component = styled(Box)`
+  display: flex;
+  margin: 55px 350px 0 300px;
+  justify-content: space-between;
+  font-family: Roboto, Arial, sans-serif;
+  font-size: 14px;
+  text-align: center;
+  font-weight: 550;
+`;
+
+//      <--------------------------------------- styled section ends-------------------------------->
+
 const NavBar = () => {
   return (
-    <Box>
+    <Component>
       {navData.map((curElem) => (
-        <Box>
-          <img src={curElem.url} alt="images" />
+        <Box key={crypto.randomUUID()}>
+          <img src={curElem.url} alt="nav image" />
           <p>{curElem.text}</p>
         </Box>
       ))}
-    </Box>
+    </Component>
   );
 };
 
