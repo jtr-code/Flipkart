88dff409dcdaf9b068df899207ad434e1ebdf825
jtr-code <jtrtricks@gmail.com>
2023-01-10 22:37:38 +0530

    Added border top shadow and bgc

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,21 @@
-import { Box } from "@mui/material";
 import NavBar from "./NavBar";
+import Banner from "./Banner";
+import { Box, styled } from "@mui/system";
+
+const Component = styled(Box)`
+  padding: 8px;
+  background-color: #f2f2f2;
+  box-shadow: inset 0px 3px 3px -3px rgba(50, 50, 50, 0.75);
+`;
 
 const Home = () => {
   return (
-    <Box>
+    <>
       <NavBar />
-    </Box>
+      <Component>
+        <Banner />
+      </Component>
+    </>
   );
 };
 
