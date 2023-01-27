803acd1ebc0c65b9cb3bec748dc4512487b04ae3
jtr-code <jtrtricks@gmail.com>
2023-01-27 16:14:51 +0530

    Added slider in home and different components

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -26,7 +26,18 @@ const Home = () => {
       <NavBar />
       <Component>
         <Banner />
-        <Slide products={products}/>
+        <Slide products={products} title="Deal of the Day" timer={true} />
+        <Slide products={products} title="Discounts for You" timer={false} />
+        <Slide products={products} title="Suggested Items" timer={false} />
+        <Slide products={products} title="Top Selection" timer={false} />
+        <Slide products={products} title="Recommended Items" timer={false} />
+        <Slide products={products} title="Trending Offers" timer={false} />
+        <Slide products={products} title="Season's Top Picks" timer={false} />
+        <Slide
+          products={products}
+          title="Top Deals on Accessories"
+          timer={false}
+        />
       </Component>
     </>
   );
