e27f0801c3efd4750d2ebedea961efd64c438700
jtr-code <jtrtricks@gmail.com>
2023-01-30 10:44:13 +0530

    singleproduct route added

--- src/components/home/Slide.jsx
+++ src/components/home/Slide.jsx
@@ -2,6 +2,7 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Box, Typography, Button, Divider, styled } from "@mui/material";
 import Countdown from "react-countdown";
+import { Link } from "react-router-dom";
 
 const responsive = {
   desktop: {
@@ -100,16 +101,21 @@ const Slide = ({ products, title, timer }) => {
         dotListClass="custom-dot-list-style"
       >
         {products.map((product) => (
-          <Box textAlign="center" style={{ padding: "25px 15px" }}>
-            <Image src={product.url} alt="product" />
-            <Text style={{ fontWeight: 600, color: "#212121" }}>
-              {product.title.shortTitle}
-            </Text>
-            <Text style={{ color: "green" }}>{product.discount}</Text>
-            <Text style={{ color: "#212121", opacity: "0.6" }}>
-              {product.tagline}
-            </Text>
-          </Box>
+          <Link
+            to={`/product/${product.id}`}
+            style={{ textDecoration: "none" }}
+          >
+            <Box textAlign="center" style={{ padding: "25px 15px" }}>
+              <Image src={product.url} alt="product" />
+              <Text style={{ fontWeight: 600, color: "#212121" }}>
+                {product.title.shortTitle}
+              </Text>
+              <Text style={{ color: "green" }}>{product.discount}</Text>
+              <Text style={{ color: "#212121", opacity: "0.6" }}>
+                {product.tagline}
+              </Text>
+            </Box>
+          </Link>
         ))}
       </Carousel>
     </Component>
