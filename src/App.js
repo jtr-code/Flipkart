6b30a0704048fa13bf5538be850d0c926f8db14a
jtr-code <jtrtricks@gmail.com>
2023-01-30 10:43:22 +0530

    added route for detailview

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import { Box } from "@mui/system";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import DataProvider from "./context/DataProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DetailView from "./components/details/DetailView";
 
 const App = () => {
   return (
     <DataProvider>
-      <Header />
-      <Box style={{ marginTop: 63 }}>
-        <Home />
-      </Box>
+      <BrowserRouter>
+        <Header />
+        <Box style={{ marginTop: 63 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<DetailView />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </DataProvider>
   );
 };
