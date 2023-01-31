811b7a830bd48bc1b67c13ba6bc3be632518e22a
jtr-code <jtrtricks@gmail.com>
2023-01-31 16:40:35 +0530

    Added single page action items

---
+++ src/components/details/ActionDetails.jsx
@@ -0,0 +1,40 @@
+import { Box, Button, styled } from "@mui/material";
+import { ShoppingCart as Cart, FlashOn as Flash } from "@mui/icons-material";
+
+const LeftContainer = styled(Box)`
+  min-width: 40%;
+  padding: 40px 0 0 80px;
+`;
+
+const Image = styled("img")({
+  padding: "15px 20px",
+  border: "1px solid #f0f0f0",
+  width: "90%",
+});
+
+const StyledButton = styled(Button)`
+  width: 48%;
+  height: 50px;
+  border-radius: 2px;
+`;
+
+const ActionDetails = ({ product }) => {
+  return (
+    <LeftContainer>
+      <Image src={product.detailUrl} alt="singleproduct" />
+      <StyledButton
+        variant="contained"
+        style={{ marginRight: 10, background: "#ff9f00" }}
+      >
+        <Cart />
+        Add to Cart
+      </StyledButton>
+      <StyledButton variant="contained" style={{ background: "#fb541b" }}>
+        <Flash />
+        Buy Now
+      </StyledButton>
+    </LeftContainer>
+  );
+};
+
+export default ActionDetails;
